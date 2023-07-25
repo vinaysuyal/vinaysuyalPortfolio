bde84dee5f2bbd8525970c9b6b6e0450fa3f4a9b
vinaysuyal <vinaysuyal2012@gmail.com>
2023-07-25 12:52:36 +0530

    Project added, visual changes made

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const projects = [
     description:
       "Designed and developed a Food Ordering Application featuring essential e-commerce functionality. The application has a visually appealing user interface and responsive design to support use on various devices, including smartphones, tablets, and desktop computers.",
     link: "https://food-order-app-sand-six.vercel.app/",
+    technologiesUsed: ["React js", "Javascript", "HTML", "CSS"],
   },
   {
     id: 2,
@@ -16,13 +17,29 @@ const projects = [
     description:
       " Developed an interactive tool to demonstrate the efficiency of backtracking algorithms in solving Sudoku puzzles. Users can observe the step-by-step process and adjust the speed for detailed examination or quick overview. A project showcasing algorithm implementation and problem-solving skills.",
     link: "https://sudoku-solver-inky.vercel.app/",
+    technologiesUsed: ["React js", "Javascript"],
   },
   {
     id: 3,
+    title: "Game of Thrones Family Tree Visualiser",
+    description:
+      "The project allows users to explore family trees of various Game of Thrones (GOT) houses. Users can select a house from a dropdown, and the corresponding family tree is displayed in an intuitive manner. The admin role has an additional feature to refresh the database with the latest character information fetched from an external API. This full-stack application showcases expertise in web development, database management, and API integration.",
+    link: "https://github.com/vinaysuyal/gotFamilyTree",
+    technologiesUsed: [
+      "React js",
+      "Spring Boot",
+      "Spring Security",
+      "Hibernate",
+      "PostgreSQL",
+    ],
+  },
+  {
+    id: 4,
     title: "Expense Tracker",
     description:
       " A user-friendly expense tracking application allowing easy entry of expenses with dates, and providing visual representations of spending patterns within specified date ranges. Efficiently manage finances and gain valuable insights for better budgeting.",
     link: "https://expense-tracker-one-nu.vercel.app/",
+    technologiesUsed: ["React js", "Javascript", "Material UI", "HTML"],
   },
 
   // Add more projects as needed
@@ -81,11 +98,25 @@ const App = () => {
         <div className="projects">
           {projects.map((project) => (
             <div key={project.id} className="project">
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <a href={project.link} target="_blank" rel="noopener noreferrer">
-                View Project
-              </a>
+              <div>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <div className="technologies-list">
+                  {project?.technologiesUsed?.map((technology) => (
+                    <span key={technology}>{technology}</span>
+                  ))}
+                </div>
+              </div>
+              <div>
+                <hr />
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Project
+                </a>
+              </div>
             </div>
           ))}
         </div>
